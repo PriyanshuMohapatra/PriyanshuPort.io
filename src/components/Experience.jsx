73dc6fc9b3a2cfae94d21750e69b068e5cc2f34b
PriyanshuMohapatra <rajacool6789@gmail.com>
2025-04-28 01:17:37 +0530

    First

---
+++ src/components/Experience.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "#1d1836",
+        color: "#fff",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className='flex justify-center items-center w-full h-full'>
+          <div className='w-4 h-4 rounded-full bg-blue-500' />
+        </div>
+      }      
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p
+          className='text-secondary text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+        {experience.certificate_link && (
+        <div className='mt-4'>
+          <a
+            href={experience.certificate_link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-400 hover:underline text-[14px]'
+          >
+            View Certificate
+          </a>
+        </div>
+      )}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far in
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Certificates.
+        </h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
---
+++ src/constants/index.js
@@ -0,0 +1,259 @@
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  // gfg,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "DevOps Engineer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Complete Interview Preparation",
+    company_name: "Geeks for Geeks",
+    iconBg: "#383E56",
+    date: "July 2024",
+    points: [
+      "Strong Foundation in DSA",
+      "Improved Problem-Solving Skills.",
+      "Increased Coding Speed & Accuracy.",
+      "Increased Coding Speed & Accuracy.",
+    ],
+    certificate_link: "https://media.geeksforgeeks.org/courses/certificates/c12e4ca90f2a9ffb05e8db35c6c6511b.pdf", 
+  },
+  {
+    title: "Server side JavaScript with Node.js",
+    company_name: "Coursera",
+    iconBg: "#383E56",
+    date: "April 2024",
+    points: [
+      "Gained hands-on experience in building server-side applications using Node.js.",
+      "Learned to create and manage RESTful APIs and handle HTTP requests/responses.",
+      "Developed skills in using Express.js for setting up routes and middleware.",
+      "Worked with MongoDB and Mongoose for database integration and data modeling.",
+    ],
+    certificate_link: "https://www.coursera.org/account/accomplishments/verify/RLPDM9Z6PYA7",
+  },
+  {
+    title: "React Basics",
+    company_name: "Coursera",
+    iconBg: "#383E56",
+    date: "April 2025",
+    points: [
+      "Practiced DevOps mindset, Agile, and Scrum.",
+      "Developed microservices-based apps.",
+      "Deployed using Docker, Kubernetes, OpenShift.",
+      "Automated CI/CD pipelines.",
+    ],
+    certificate_link: "https://www.coursera.org/account/accomplishments/verify/5VR9RPQSKV6L",
+  },
+  {
+    title: "Cloud Computing",
+    company_name: "NPTEL",
+    iconBg: "#383E56",
+    date: "October 2024",
+    points: [
+      "Learned cloud service and deployment models (IaaS, PaaS, SaaS).",
+      "Understood virtualization and cloud infrastructure.",
+      "Understood architecture of cloud platforms and data centers.",
+    ],
+    certificate_link: "https://archive.nptel.ac.in/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL24CS118S167020201804432022.pdf",
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+
+export { services, technologies, experiences, testimonials, projects };
